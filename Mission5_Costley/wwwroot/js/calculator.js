e093e264ec904dfdccb329b6af7bb0197e5e7567
lorincostley <lorin.costley@gmail.com>
2024-02-07 21:01:41 -0700

    added non-editable input box

--- Mission5_Costley/wwwroot/js/calculator.js
+++ Mission5_Costley/wwwroot/js/calculator.js
@@ -1,12 +1,11 @@
 ﻿$(document).ready(function () {
-
     //Calculator functionality
     $('#calculate').click(function () {
         var hours = parseFloat($('#hours').val());
         if (!isNaN(hours) && hours > 0) {
             var chargePerHour = 20;
             var total = hours * chargePerHour;
-            $('#total').text('$' + total.toFixed(2));
+            $('#total').val('$' + total.toFixed(2));
         } else {
             alert('Please enter a valid number of hours.');
         }
